37382494cc37d7e63bb4d46dbc4cf3f2baabbb99
rajantamang1 <62918494+rajantamang1@users.noreply.github.com>
2020-09-29 18:58:35 -0700

    finally working easy level

---
+++ challenge_tues.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import argparse, socket
+from datetime import datetime
+
+# answer= " "
+#
+# while answer != "q":
+#   try:
+#     char_name= input("Which character do you want to know about? (Flash, Batman, Superman) ").lower()
+#
+#     char_stat= input("What statistic do you want to know about? (strength, speed, or intelligence) ").lower()
+#
+#     print(f"{char_name.capitalize()}'s {char_stat} is: {main_dictionary[char_name][char_stat].capitalize()}")
+#   except:
+#     print("You provided incorrect input.")
+#
+#   answer= input("Press ENTER to choose another hero, or press Q to quit!").lower()
+
+
+
